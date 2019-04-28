5f8e4dc608a00a2f4a855a2f73fa59d99daa08f6
Rick Fontein <r.fontein.95@gmail.com>
2019-04-28 23:49:19 +0200

    GOT Timezone tinkery

--- src/main/java/com/jagrosh/jmusicbot/got/GOTManager.java
+++ src/main/java/com/jagrosh/jmusicbot/got/GOTManager.java
@@ -18,10 +18,12 @@ public class GOTManager {
     private final String guildID = "276858200853184522"; //slap
     private final String channelID = "570699432941584412"; //slap
     private final Date[] episodeDates = {
-            new Date(119, Calendar.APRIL, 29, 2, 0, 0),
-            new Date(119, Calendar.MAY, 6, 2, 0, 0),
-            new Date(119, Calendar.MAY, 13, 2, 0, 0),
-            new Date(119, Calendar.MAY, 20, 2, 0, 0)
+            new Date(119, Calendar.APRIL, 15, 3, 0, 0),
+            new Date(119, Calendar.APRIL, 22, 3, 0, 0),
+            new Date(119, Calendar.APRIL, 29, 3, 0, 0),
+            new Date(119, Calendar.MAY, 6, 3, 0, 0),
+            new Date(119, Calendar.MAY, 13, 3, 0, 0),
+            new Date(119, Calendar.MAY, 20, 3, 0, 0)
     }; //year is 1900 + 119 = 2019
 
 
@@ -33,46 +35,38 @@ public class GOTManager {
     };
      */
 
-    private final String WARNING =
-            "**Huzzah, tonight another episode of GoT!**\n" +
-                    "\n" +
-                    "This channel will lock before the episode (around 2AM BST)\n" +
-                    "And unlock the day after around midday.\n" +
-                    "\n" +
-                    "Some things to note:\n" +
-                    "- Mute this chat by clicking the bell icon (avoids spoiler notifications)\n" +
-                    "- Spoilers of the __released__ episode are allowed when the channel unlocks.\n" +
-                    "- Posting GoT content anywhere else will earn you a kick from the server** :heart:\n" +
-                    "@here";
+    private final String WARNING = "" +
+            "**:tv: Huzzah, tonight another episode of GoT!**\n" +
+            "\n" +
+            "This channel will lock before the episode (around 2AM BST)\n" +
+            "And unlocks around 6PM (BST).\n" +
+            "\n" +
+            "Some things to note:\n" +
+            "- Mute this chat by clicking the :bell: icon (avoids spoiler notifications)\n" +
+            "- Spoilers of the __released__ episode are allowed when the channel unlocks.\n" +
+            "- Posting GoT content anywhere else will earn you a ban from the server :hammer:\n";
     private final String LOCK =
-            "**:lock: The new episode is about to be released. This channel is now locked!**\n";
+            "**:lock: The new episode is about to be released. This channel is now locked till 6PM (BST)!**\n";
     private final String UNLOCK =
             "**:unlock: This channel is now unlocked! Meme away.**\n";
 
 
     public GOTManager(Bot bot) {
-
-
         JDA jda = bot.getJDA();
         if (jda == null) {
             System.out.println("jda");
             return;
         }
-        jda.getGuilds().forEach(System.out::println);
-
-
         TextChannel tc = jda.getTextChannelById(channelID);
         if (tc == null) {
             System.out.println("tc");
             return;
         }
-
         Guild guild = jda.getGuildById(guildID);
         if (guild == null) {
             System.out.println("guild");
             return;
         }
-
         Role everyone = guild.getPublicRole();
         if (everyone == null) {
             System.out.println("everyone");
@@ -80,47 +74,47 @@ public class GOTManager {
         }
 
 
-        tc.sendMessage("Hi nerds.\n").queue();
-
         Date now = new Date();
+        System.out.println("Now | " + now.toString());
         for (Date episode : episodeDates) {
-            System.out.println(episode.toString());
-
-            long warndelay = episodeDates[0].getTime() - now.getTime() - 14400000;
-            long lockdelay = episodeDates[0].getTime() - now.getTime();
-            long unlockdelay = episodeDates[0].getTime() - now.getTime() + 36000000;
-
-            /* test
-            long warndelay = episode.getTime() - now.getTime() - 30000;
-            long lockdelay = episode.getTime() - now.getTime();
-            long unlockdelay = episode.getTime() - now.getTime() + 30000;
-            */
 
             //Warning
+            long warndelay = episode.getTime() - 14400000 - now.getTime();
             if (warndelay > 0) {
+                System.out.println("Warn   | " + new Date(episode.getTime() - 14400000).toString());
                 ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
                 ses.schedule(() -> {
                     tc.sendMessage(WARNING).queue();
                 }, warndelay, TimeUnit.MILLISECONDS);
+            } else {
+                System.out.println("Warn   | " + new Date(episode.getTime() - 14400000).toString() + " SKIPPED");
             }
 
             //lock
+            long lockdelay = episode.getTime() - now.getTime();
             if (lockdelay > 0) {
+                System.out.println("Lock   | " + new Date(episode.getTime()).toString());
                 ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
                 ses.schedule(() -> {
                     tc.sendMessage(LOCK).queue();
                     tc.putPermissionOverride(everyone).setDeny(Permission.MESSAGE_WRITE).queue();
                 }, lockdelay, TimeUnit.MILLISECONDS);
+            } else {
+                System.out.println("Lock   | " + new Date(episode.getTime()).toString() + " SKIPPED");
             }
 
 
             //unlock
+            long unlockdelay = episode.getTime() + 57600000 - now.getTime();
             if (unlockdelay > 0) {
+                System.out.println("Unlock | " + new Date(episode.getTime() + 57600000).toString());
                 ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
                 ses.schedule(() -> {
                     tc.sendMessage(UNLOCK).queue();
                     tc.putPermissionOverride(everyone).setAllow(Permission.MESSAGE_WRITE).queue();
                 }, unlockdelay, TimeUnit.MILLISECONDS);
+            } else {
+                System.out.println("Unlock | " + new Date(episode.getTime() + 57600000).toString() + " SKIPPED");
             }
         }
     }
